237eadbedc478935cb869c9ba452b66327e00dbc
NRG Development <gurpreetsingh.19842008@gmail.com>
2026-03-15 21:00:06 +0000

    Fixed some issues with the UI

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 name 'rs-bossmenu'
 author 'NRG development'
 description 'Premium boss menu with modern NUI, society banking, employee management, and job utilities'
-version '1.0.0'
+version '1.0.1'
 
 ui_page 'web/index.html'
 
